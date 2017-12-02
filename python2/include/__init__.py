c266fc980a5b3b1c5e036a7ccb88cb87c1a98dca
Grandh <296493452@qq.com>
2017-12-02 23:14:12 +0800

    v0.1 commited by huang, includes some programs for test and guidance in wiki

---
+++ python2/include/__init__.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+from resizeImage import *
+
+if __name__ == "__main__":
+    resizeImage("../images/face-test-image/wuyanzu.jpg",128,128)
---
+++ python2/test/__init__.py
@@ -0,0 +1,40 @@
+# coding:utf-8 
+import cv2
+from PIL import Image
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+'''
+- 测试摄像头和配置环境
+- 在当前目录下执行 python __init__.py 0
+- 上面的0对应opencv中camera的id，作为系统参数sys.argv[1]输入
+'''
+
+def CatchUsbVideo(window_name, camera_idx):
+    cv2.namedWindow(window_name)
+    
+    # 视频来源
+    # 可以直接来自USB摄像头，也可换为视频
+    cap = cv2.VideoCapture(camera_idx)        
+        
+    while cap.isOpened():
+        ok, frame = cap.read() #读取一帧数据
+        if not ok:break                    
+                        
+        #显示图像并等待10毫秒按键输入，输入‘q’退出程序
+        cv2.imshow(window_name, frame)
+        c = cv2.waitKey(10)
+        if c & 0xFF == ord('q'):
+            break        
+    
+    #释放摄像头并销毁所有窗口
+    cap.release()
+    cv2.destroyAllWindows() 
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("无效的摄像头id:%s\r\n" % (sys.argv[0]))
+    else:
+        CatchUsbVideo("Vedio", int(sys.argv[1]))
